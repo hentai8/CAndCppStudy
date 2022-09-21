1cdf0b1de21576566018e8b56d5d45113ec6a70c
hentai8 <496983136@qq.com>
2022-09-21 10:16:37 +0800

    feature: hello-world.cu

---
+++ basic/test1.cpp
@@ -0,0 +1,14 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N = 1 << 20;
+//    printf(String(N));
+    cout << N << endl;
+    cout << "123" << endl;
+    return 0;
+}

fbd96ae8d1fa316ac1b3c2bd2a5bad8a62e6d032
hentai8 <496983136@qq.com>
2022-09-21 10:18:47 +0800

    feature: C study init

---
+++ CStudy/log/test.c
@@ -0,0 +1,13 @@
+#include "log.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+int main()
+{
+    char str[] = "123";
+    printf(str,"123");
+    LOG("write_new_block job:","\n");
+}
---
+++ CStudy/struct/test.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <atomic>
+
+
+struct structB {
+    int b;
+    ssize_t c;
+};
+
+struct structA {
+    int a;
+    std::atomic<uint32_t> ref_count;
+    structB b;
+};
+void print_structA(struct structA *structa);
+
+
+int main()
+{
+    printf("000");
+    struct structA structa;
+    structa.a = 123;
+    structa.b.b = 999;
+    print_structA(&structa);
+    printf("321");
+    // print_structA()
+    return 0;
+}
+
+void print_structA(struct structA *struct0){
+    printf("Book title : %d\n",struct0->a);
+    printf("Book title : %d\n",struct0->b.b);
+    printf("sszie_t : %zd\n",struct0->b.c);
+    printf("ref_count: %u\n",struct0->ref_count.load());
+}

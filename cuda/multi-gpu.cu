100a80ef1566f2c0199558ad74aa1ba90f9e74ee
hentai8 <496983136@qq.com>
2022-10-11 15:53:34 +0800

    feature: 获取多gpu的max grid & max block

--- cuda/multi-gpu.cu
+++ cuda/multi-gpu.cu
@@ -30,11 +30,19 @@ __global__ void add(int n, float *x, float *y)
         y[i] = x[i] + y[i];
 }
 
+int get_device_cores(int device_id);
+void config_cuda(int device_id, int *grid_size, int *block_size, bool *is_inline_miner);
+inline int get_sm_cores(int major, int minor);
+
+
+
 int main(void)
 {
     int N = 1 << 20; // 1M elements
 
     // Get the numble of CUDA-capble GPU
+
+    // 获取一共有多少个GPU
     int N_GPU;
     cudaGetDeviceCount(&N_GPU);
 
@@ -46,11 +54,21 @@ int main(void)
     TGPUplan plan[N_GPU];
 
     // Initializing
+    // 对于每个GPU，分别进行处理
     for (int i = 0; i < N_GPU; i++)
     {
+        int id = i;
+        int device_id = i;
         cudaSetDevice(i);
+        cudaSetDevice(device_id);
         cudaStreamCreate(&plan[i].stream);
-
+        int grid_size;
+        int block_size;
+        bool is_inline_miner;
+        is_inline_miner = true;
+        config_cuda(device_id, &grid_size, &block_size, &is_inline_miner);
+        printf("Worker %d: device id %d, grid size %d, block size %d. Using %s kernel\n", id, device_id,
+            grid_size, block_size, is_inline_miner ? "inline" : "reference");
         cudaMalloc((void **)&plan[i].d_x, Np * sizeof(float));
         cudaMalloc((void **)&plan[i].d_y, Np * sizeof(float));
         plan[i].h_x = (float *)malloc(Np * sizeof(float));
@@ -113,4 +131,103 @@ int main(void)
     std::cout << "Max error: " << maxError << std::endl;
 
     return 0;
+}
+
+void config_cuda(int device_id, int *grid_size, int *block_size, bool *is_inline_miner)
+{
+    cudaSetDevice(device_id);
+
+    cudaDeviceProp props;
+    cudaGetDeviceProperties(&props, device_id);
+
+    // If using a 2xxx or 3xxx card, use the new grid calc
+    bool use_rtx_grid_bloc = ((props.major << 4) + props.minor) >= 0x75;
+
+    // If compiling for windows, override the test and force the new calc
+#ifdef _WIN32
+    use_rtx_grid_bloc = true;
+#endif
+
+    // If compiling for linux, and we're not using the RTX grid block, force the original miner, otherwise use the inlined one
+#ifdef __linux__
+    *is_inline_miner = use_rtx_grid_bloc;
+#else
+    *is_inline_miner = true;
+#endif
+    // if(*is_inline_miner) {
+    //     cudaOccupancyMaxPotentialBlockSize(grid_size, block_size, inline_blake::blake3_hasher_mine);
+    // } else {
+    //     cudaOccupancyMaxPotentialBlockSize(grid_size, block_size, ref_blake::blake3_hasher_mine);
+    // }
+
+    int cores_size = get_device_cores(device_id);
+    if (use_rtx_grid_bloc)
+    {
+        *grid_size = props.multiProcessorCount * 2;
+        *block_size = cores_size / *grid_size * 4;
+    }
+    else
+    {
+        *grid_size = cores_size / *block_size * 3 / 2;
+    }
+}
+
+int get_device_cores(int device_id)
+{
+    cudaDeviceProp props;
+    cudaGetDeviceProperties(&props, device_id);
+
+    int cores_size = get_sm_cores(props.major, props.minor) * props.multiProcessorCount;
+    return cores_size;
+}
+
+// Beginning of GPU Architecture definitions
+inline int get_sm_cores(int major, int minor)
+{
+    // Defines for GPU Architecture types (using the SM version to determine
+    // the # of cores per SM
+    typedef struct
+    {
+        int SM; // 0xMm (hexidecimal notation), M = SM Major version,
+        // and m = SM minor version
+        int Cores;
+    } sSMtoCores;
+
+    sSMtoCores nGpuArchCoresPerSM[] = {
+        {0x30, 192},
+        {0x32, 192},
+        {0x35, 192},
+        {0x37, 192},
+        {0x50, 128},
+        {0x52, 128},
+        {0x53, 128},
+        {0x60, 64},
+        {0x61, 128},
+        {0x62, 128},
+        {0x70, 64},
+        {0x72, 64},
+        {0x75, 64},
+        {0x80, 64},
+        {0x86, 128},
+        {-1, -1}};
+
+    int index = 0;
+
+    while (nGpuArchCoresPerSM[index].SM != -1)
+    {
+        if (nGpuArchCoresPerSM[index].SM == ((major << 4) + minor))
+        {
+            return nGpuArchCoresPerSM[index].Cores;
+        }
+
+        index++;
+    }
+
+    // If we don't find the values, we default use the previous one
+    // to run properly
+    printf(
+        "MapSMtoCores for SM %d.%d is undefined."
+        "  Default to use %d Cores/SM\n",
+        major, minor, nGpuArchCoresPerSM[index - 1].Cores);
+    return nGpuArchCoresPerSM[index - 1].Cores;
 }
